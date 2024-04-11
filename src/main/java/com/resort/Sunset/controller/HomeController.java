79e1f6cb6609339443a076573f3b89b15b7b0d21
yeheeshin <yehee7894@naver.com>
2024-04-11 19:28:54 +0900

    url - controller 연결

--- src/main/java/com/resort/Sunset/controller/HomeController.java
+++ src/main/java/com/resort/Sunset/controller/HomeController.java
@@ -8,4 +8,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor
 public class HomeController {
 
+    @GetMapping("/room")
+    public String room() {
+        return "/rooms";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "/about-us";
+    }
+
+    @GetMapping("/roomDetail")
+    public String roomDetail() {
+        return "/room-details";
+    }
+
+    @GetMapping("/blog")
+    public String blog() {
+        return "/blog";
+    }
+
+    @GetMapping("/blogDetail")
+    public String blogDetail() {
+        return "/blog-details";
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "/contact";
+    }
+
 }
