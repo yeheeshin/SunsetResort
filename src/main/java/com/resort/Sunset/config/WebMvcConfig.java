2d9c3e5d6ee5690b52a522b099003b56f7f0cd74
yeheeshin <zia7894@naver.com>
2024-05-27 09:28:54 +0900

    파일 경로 설정 수정

--- src/main/java/com/resort/Sunset/config/WebMvcConfig.java
+++ src/main/java/com/resort/Sunset/config/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
         WebMvcConfigurer.super.addResourceHandlers(registry);
 
         // 외부의 있는 폴더를 내 프로젝트에서 찾을 수 있도록 세팅
-        registry.addResourceHandler("/gallery/**")
+        registry.addResourceHandler("/sunset_gallery/**")
                 .addResourceLocations("file:///" + MyPath.filePath)
                 .setCachePeriod(60 * 10 * 6)// 서버 부하를 줄이기 위해 해당요청을 캐시에 저장해놓는 시간을 지정
                 .resourceChain(true)
