79d9cca9886f14707ef490c7013f23c23fe9dbcc
yeheeshin <zia7894@naver.com>
2024-05-29 08:54:03 +0900

    다중 이미지 업로드 가능

--- src/main/java/com/resort/Sunset/controller/FileController.java
+++ src/main/java/com/resort/Sunset/controller/FileController.java
@@ -8,8 +8,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -19,11 +21,14 @@ public class FileController {
 
 
     @PostMapping("fileUp")
-    public String fileUpload(@ModelAttribute("allFile") fileForm fileData) {
+    public String fileUpload(fileForm fileData) {
 
         try {
-            Map<String, Object> fileAllData = fileForm.uploadFileName(fileData);
-            roomService.insertImg(fileAllData);
+            List<MultipartFile> files = fileData.getFile();// -> list
+            for (MultipartFile file : files) {
+                Map<String, Object> fileAllData = fileForm.uploadFileName(file,fileData);
+                roomService.insertImg(fileAllData);
+            }
 
             return "redirect:/room";
         } catch (IOException e) {
--- src/main/java/com/resort/Sunset/form/fileForm.java
+++ src/main/java/com/resort/Sunset/form/fileForm.java
@@ -10,22 +10,23 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
 @Getter
 @Setter
 public class fileForm {
-    private MultipartFile file;
+    private List<MultipartFile> file;
     private Long pro_id;
     private Long res_id;
     private Long room_id;
     private Long store_id;
 
-    public static Map<String, Object> uploadFileName(fileForm fileForm) throws IOException {
-        String filename = fileForm.getFile().getOriginalFilename();
+    public static Map<String, Object> uploadFileName(MultipartFile listFile,fileForm fileForm) throws IOException {
+        String filename = listFile.getOriginalFilename();
         Path filePath = Paths.get(MyPath.filePath + filename);
-        Files.write(filePath, fileForm.getFile().getBytes());
+        Files.write(filePath, listFile.getBytes());
 
         UUID uuid = UUID.randomUUID();
         String fileName = uuid + "_" + filename;
