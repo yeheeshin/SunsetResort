48ff2a64160374e0a63f49923d4b66c24d2f30a8
yeheeshin <zia7894@naver.com>
2024-06-12 17:24:07 +0900

    예약하기 페이지 이동 , css 수정

--- src/main/java/com/resort/Sunset/controller/HomeController.java
+++ src/main/java/com/resort/Sunset/controller/HomeController.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -63,7 +64,7 @@ public class HomeController {
         return "/ex";
     }
 
-    @GetMapping("/roomRes")
+    @PostMapping("/roomRes")
     public String roomRes() {
         return "/roomRes";
     }
