6f191507ef06ed31e0cfa3576506bb852fd05949
yeheeshin <zia7894@naver.com>
2024-06-17 19:53:39 +0900

    nav : url 설정

--- src/main/java/com/resort/Sunset/config/SecurityConfig.java
+++ src/main/java/com/resort/Sunset/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig{
                 )
                 .logout(logout ->
                         logout
-                                .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
+                                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                                 .logoutSuccessUrl("/")
                                 .invalidateHttpSession(true)
                                 .deleteCookies("JSESSIONID")
