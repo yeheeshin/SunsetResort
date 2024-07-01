71d7c339fad59c2e1500f4780c104031e23921bd
yeheeshin <zia7894@naver.com>
2024-07-01 13:21:25 +0900

    마이페이지 - 예약 목록 띄우기

--- src/main/java/com/resort/Sunset/controller/MyPageController.java
+++ src/main/java/com/resort/Sunset/controller/MyPageController.java
@@ -1,7 +1,9 @@
 package com.resort.Sunset.controller;
 
+import com.resort.Sunset.dto.room;
 import com.resort.Sunset.dto.room_reserve;
 import com.resort.Sunset.service.RoomReserveService;
+import com.resort.Sunset.service.RoomService;
 import com.resort.Sunset.service.UserService;
 import lombok.RequiredArgsConstructor;
 
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -17,12 +20,23 @@ public class MyPageController {
 
     private final RoomReserveService roomReserveService;
     private final UserService userService;
+    private final RoomService roomService;
 
     @GetMapping("/myPage")
     public String myPage(Model model) {
+        List<room> roomInfo = new ArrayList<>();
+
         List<room_reserve> resById = roomReserveService.getResById(userService.nowUser().getUser_id());
 
+        for (room_reserve res : resById) {
+            Long roomId = res.getRoom_id();
+
+            room room = roomService.getRoom(roomId);
+            roomInfo.add(room);
+        }
+
         model.addAttribute("resList", resById);
+        model.addAttribute("roomInfo", roomInfo);
 
         return "/myPage";
     }
