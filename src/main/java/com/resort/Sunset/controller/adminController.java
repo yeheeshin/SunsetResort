1ebc03f7576063e13c7a9a0e2cc26fa087bf1a7a
yeheeshin <zia7894@naver.com>
2024-07-22 15:38:57 +0900

    관리자 페이지 틀

---
+++ src/main/java/com/resort/Sunset/controller/adminController.java
@@ -0,0 +1,16 @@
+package com.resort.Sunset.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class adminController {
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "/admin/admin";
+    }
+}
+
